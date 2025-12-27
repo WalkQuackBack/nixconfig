fd55cd2eef8a1bd214bcc74331b9ccb65e3c3584
WalkQuackBack <quacksareback@duck.com>
2025-12-26 19:39:45 -0700

    feat(home-manager/plasma): add keyboard shortcuts Adds empty Google Chrome shortcuts, and binds Walk Through WIndows main to Alt+Tab and the alternative to Meta+Tab, and the same inverse.

--- modules/home-manager/plasma.nix
+++ modules/home-manager/plasma.nix
@@ -147,5 +147,38 @@ in
         };
       };
     };
+
+    shortcuts = {
+      "com.google.Chrome" = {
+        "53585719B19B025CF4ABFF5F729C56CE-open-dashboard" = [ ];
+        "53585719B19B025CF4ABFF5F729C56CE-open-dashboard-with-running-scripts" = [ ];
+        "53585719B19B025CF4ABFF5F729C56CE-open-new-script" = [ ];
+        "53585719B19B025CF4ABFF5F729C56CE-toggle-enable" = [ ];
+        "757BF25F81655CFB52717389EC3122D1-addSite" = [ ];
+        "757BF25F81655CFB52717389EC3122D1-switchEngine" = [ ];
+        "757BF25F81655CFB52717389EC3122D1-toggle" = [ ];
+        AE8B425B2EBD43CBB24B4A8AF1A58532-autofill_card = [ ];
+        AE8B425B2EBD43CBB24B4A8AF1A58532-autofill_identity = [ ];
+        AE8B425B2EBD43CBB24B4A8AF1A58532-autofill_login = [ ];
+        AE8B425B2EBD43CBB24B4A8AF1A58532-generate_password = [ ];
+        AE8B425B2EBD43CBB24B4A8AF1A58532-lock_vault = [ ];
+        EA5240BAFDBA0429571F7DE323020F6B-openManage = [ ];
+        EA5240BAFDBA0429571F7DE323020F6B-reload = [ ];
+        EA5240BAFDBA0429571F7DE323020F6B-styleDisableAll = [ ];
+        FFB46232F737A83A70A1270EAA4EAA56-enter-picker-mode = [ ];
+        FFB46232F737A83A70A1270EAA4EAA56-enter-zapper-mode = [ ];
+      };
+
+      kwin = {
+        "Walk Through Windows" = ["Alt+Tab" ""];
+        "Walk Through Windows (Reverse)" = ["Alt+Shift+Tab" ""];
+        "Walk Through Windows Alternative" = ["Meta+Tab" ""];
+        "Walk Through Windows Alternative (Reverse)" = ["Meta+Shift+Tab" ""];
+        "Walk Through Windows of Current Application" = ["Meta+`" "Alt+`"];
+        "Walk Through Windows of Current Application (Reverse)" = ["Meta+~" "Alt+~"];
+        "Walk Through Windows of Current Application Alternative" = [ ];
+        "Walk Through Windows of Current Application Alternative (Reverse)" = [ ];
+      };
+    };
   };
 }
