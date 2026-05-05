80a1c11de4ea6bce6880f780f4b900e7e863628a
WalkQuackBack <quacksareback@duck.com>
2026-05-04 20:17:52 -0600

    chore(packages/lutris): change from wineWowPackages to wineWow64Packages

--- modules/packages/lutris.nix
+++ modules/packages/lutris.nix
@@ -2,7 +2,7 @@
     environment.systemPackages = with pkgs; [
         (lutris.override {
             extraPkgs = pkgs: [
-                wineWowPackages.waylandFull
+                wineWow64Packages.waylandFull
                 winetricks
                 vulkan-tools
                 xterm
